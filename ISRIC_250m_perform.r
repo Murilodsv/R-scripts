14c9edee5af67fc6535b901cd0ae9d5b55a7c5cb
Murilodsv <murilodsv@usp.br>
2018-04-09 22:15:12 -0300

    Read_write all dbf

--- ISRIC_250m_perform.r
+++ ISRIC_250m_perform.r
@@ -13,45 +13,55 @@ dbf_df   = read.table(text = dbf_list,sep="_",colClasses = "character",fill = T)
 colnames(dbf_df) = c("tif","m","sl","res","pe_id1","pe_id2","pe_id3","pe_id4","zband","clustermethod","dateofzoning")
 dbf_df$filename = dbf_list
 
+#--- Remove noData dbf files from list
+unique(dbf_df$pe_id3)
+dbf_df = dbf_df[!(dbf_df$pe_id3=="Horta964917"),]   # Remove Horta from Santa Ernestina
+dbf_df = dbf_df[!(dbf_df$pe_id2=="SaoFrancisco"),]  # Remove subfield from Sao Francisco
+dbf_df = dbf_df[!(dbf_df$pe_id2=="Rodeio" & dbf_df$pe_id3=="Subfield2976547"),]  # Remove subfield from Rodeio due to field "ZONE"
+dbf_df = dbf_df[!(dbf_df$pe_id2==""),]  # T01 02 03 04...
 
 length(dbf_df$res)
+
 #--- Load library for DBF import
 library(foreign)
 
-#--- Read DBF
+#--- Unique names by tif type
 utif = unique(dbf_df$tif)
 
-#--- Clay files
-dbfcl_ls = dbf_df$filename[dbf_df$tif=="CLYPPT"]
-
-dbfrd = dbfcl_ls
-
-dbf_files_df = lapply(dbfrd,function(x){read.dbf(file = x, as.is = F); print(x)})
-combined_dbf = do.call("rbind", lapply(dbf_files_df, as.data.frame))
-
-combined_dbf
+#--- Read and gather all dbf files
+lapply(utif,wcsv)
 
 
-length(dbf1$X_mean) + length(dbf2$X_mean)
+#-----------------#
+#--- Functions ---#
+#-----------------#
 
-length(combined_df$X_mean)
+#--- Gather dbf files and write as .csv
+wcsv = function(y){
+  dbf_ls = dbf_df$filename[dbf_df$tif==y]
+  dbf    = rdbfsev(dbf_ls)
+  write.csv(dbf, file = paste(y,".csv",sep=""), quote = F)
+  
+}
 
-help(lapply)
+#--- Function Read Several dbf
+rdbfsev = function(z){
+  dbf_files_df = lapply(z,rdbf)
+  rdbfsev = do.call("rbind", lapply(dbf_files_df, as.data.frame))
+  rdbfsev
+}
 
-dbf1$X_mean
-dbf2$X_mean
-
-dbf
-
-length(dbf_list)
-
-tail(dbf_list)
-
-dbf_list[1000:1607]
+#--- Read DBF function
+rdbf = function(x) {
+  f = read.dbf(file = x, as.is = F)
+  f$filename = x
+  f$ACRON = NULL #Remove this additional field (added on Qgis only for layout)
+  f
+}
 
 
-read.delim(file = dbf_list, sep = "_")
+#Check col names
+#cnames = lapply(dbf_files_df, function(x) colnames(x))
 
-apropos("read")
 
-help("read.delim")
+
