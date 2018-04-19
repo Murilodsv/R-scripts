1b3613886bc583571aaff94c9f1ac695d879c2be
Murilodsv <murilodsv@usp.br>
2018-04-19 17:33:19 -0300

    mperf

--- SWAP_samuca.R
+++ SWAP_samuca.R
@@ -87,9 +87,119 @@ wstr$year = as.factor(format(as.Date(wstr$Date, format="%d-%b-%Y"),"%Y"))
 wstr$doy  = as.factor(yday(as.Date(wstr$Date, format="%d-%b-%Y")))
 
 #--- Include das in all them
-atm = inx(atm)    #Atmosphere
-swba= inx(swba)   #Soil Water Balance
-wstr= inx(wstr)   #Water stresses
+atm       = inx(atm)    #Atmosphere
+swba      = inx(swba)   #Soil Water Balance
+wstr      = inx(wstr)   #Water stresses
+
+#------------------------#
+#--- Machine Learning ---#
+#------------------------#
+
+#--- Compute statistical indexes of performance
+
+#--- seting the simulated depths as equal to FDR depths measurements (10, 20, 30, 60)
+dsim = data.frame(fdr = colnames(fdr)[4:7], depth = c(-10,-19.5,-28.5,-58.5))
+l = merge(swba,dsim,by = "depth")
+l = l[order(l$das),]              #--- sort by das
+
+sim_swc10  = l[l$fdr==colnames(fdr)[4],c("das","wcontent")]
+sim_swc20  = l[l$fdr==colnames(fdr)[5],c("das","wcontent")]
+sim_swc30  = l[l$fdr==colnames(fdr)[6],c("das","wcontent")]
+sim_swc60  = l[l$fdr==colnames(fdr)[7],c("das","wcontent")]
+sim_swc    = data.frame(das = sim_swc10$das,sim_swc10 = sim_swc10$wcontent, sim_swc20 = sim_swc20$wcontent, sim_swc30 = sim_swc30$wcontent, sim_swc60 = sim_swc60$wcontent)
+
+so_fdr = merge(fdr,sim_swc, by = "das")
+
+s_swc10  = so_fdr$sim_swc10
+s_swc20  = so_fdr$sim_swc20
+s_swc30  = so_fdr$sim_swc30
+s_swc60  = so_fdr$sim_swc60
+
+o_swc10  = so_fdr$fdr10cm
+o_swc20  = so_fdr$fdr20cm
+o_swc30  = so_fdr$fdr30cm
+o_swc60  = so_fdr$fdr60cm
+
+s_swc = c(s_swc10,s_swc20,s_swc30,s_swc60)
+o_swc = c(o_swc10,o_swc20,o_swc30,o_swc60)
+
+
+vl = "SWC (cm3 cm-3)"
+
+par(mfrow=c(1,1), mar = c(4.5, 4.5, 0.5, 0.5), oma = c(0, 0, 0, 0))
+
+
+p_swc10  = mperf(s_swc10,o_swc10,vl)
+p_swc20  = mperf(s_swc20,o_swc20,vl)
+p_swc30  = mperf(s_swc30,o_swc30,vl)
+p_swc60  = mperf(s_swc60,o_swc60,vl)
+p_swc    = mperf(s_swc,o_swc,vl)
+
+
+mperf = function(sim,obs,vnam){
+
+  # sim  - simulated values [R]
+  # obs  - observed values [R]
+  # vnam - Name of variable as string for chart axis  [S]
+  
+#--- Statistical indexes
+fit   = lm(sim~obs)
+bias  = (1/length(obs)) * sum(sim-obs)
+mse   = (1/length(obs)) * sum((sim-obs)^2)
+rmse  = sqrt(mse)
+mae   = (1/length(obs)) * sum(abs(sim-obs))
+rrmse = rmse / mean(obs)
+rmae  = (1/length(obs)) * sum(abs(sim-obs)/abs(obs))
+ef    = 1 - (sum((sim-obs)^2) / sum((obs-mean(obs))^2))
+r     = sum((obs-mean(obs))*(sim-mean(sim)))/sqrt(sum((obs-mean(obs))^2)*sum((sim-mean(sim))^2))
+r2    = r^2
+d     = 1 - (sum((sim-obs)^2) / sum((abs(sim-mean(obs))+abs(obs-mean(obs)))^2))
+a     = summary(fit)$coefficients["(Intercept)","Estimate"]
+b     = summary(fit)$coefficients["obs","Estimate"]
+
+#--- Chart Sim ~ Obs
+varlab = vnam 
+
+mindt = min(obs,sim)
+maxdt = max(obs,sim)
+#--- Ploting limits 
+pllim = c(mindt-0.1*(maxdt-mindt),maxdt+0.1*(maxdt-mindt))
+xx = seq(min(obs),max(obs),length = (max(obs)-min(obs)))
+z = summary(fit)
+
+plot(sim~obs,
+     ylab = paste("Sim - ",varlab,sep = ""),
+     xlab = paste("Obs - ",varlab,sep = ""),
+     ylim = pllim,
+     xlim = pllim)
+
+lines(xx, predict(fit, data.frame(obs=xx)),
+      col = "black",
+      lty = 1,
+      lwd = 1.5)
+
+l11 = seq(pllim[1]-0.5*pllim[1], pllim[2] + 0.5 * pllim[2],length = 1000)
+
+lines(l11*1~l11,
+      col = "red",
+      lty = 2,
+      lwd = 1.5)
+
+perf = data.frame(bias,
+                  mse,
+                  rmse,
+                  mae,
+                  rrmse,
+                  rmae,
+                  ef,
+                  r,
+                  r2,
+                  d,
+                  a,
+                  b)
+
+}
+
 
 #------------------------#
 #-------- Charts --------#
@@ -97,7 +207,7 @@ wstr= inx(wstr)   #Water stresses
 #------------------------#
 
 #--- seting the simulated depths as equal to FDR depths measurements (10, 20, 30, 60)
-dsim = data.frame(fdr = colnames(fdr)[4:7], depth = c(-10,-19.5,-31.5,-58.5))
+dsim = data.frame(fdr = colnames(fdr)[4:7], depth = c(-10,-19.5,-28.5,-58.5))
 
 #--- Separating data for lines
 l = merge(swba,dsim,by = "depth")
